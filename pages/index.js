6882828350fa84572ec70c8049a1c0a11e93a434
Mayco Barale <maycobarale@gmail.com>
2023-02-07 18:24:22 -0300

    add basic layou to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Image from "next/image";
 import data from "../data.json";
 
-export default function Home() {
-  const { name, avatar } = data;
+function LinkCard({ href, title, image }) {
   return (
-    <div className="flex flex-col items-center">
+    <a
+      href={href}
+      className="p-1 border border-gray-300 w-full mb-3 rounded-sm hover:scale-105 transition-all"
+    >
+      <div className="flex">
+        {image && (
+          <Image className="" src={image} alt={title} width={40} height={40} />
+        )}
+        <h2>{title}</h2>
+      </div>
+    </a>
+  );
+}
+
+export default function Home() {
+  const { name, avatar, links } = data;
+  return (
+    <div className="flex flex-col items-center  h-screen mt-16 mx-8">
       <Image
         className="rounded-full"
         src={avatar}
@@ -12,7 +28,10 @@ export default function Home() {
         width={96}
         height={96}
       />
-      <h1 className="font-bold text-2xl">{name}</h1>
+      <h1 className="font-semibold text-2xl mt-4 mb-8">{name}</h1>
+      {links.map((link) => (
+        <LinkCard key={link.title} {...link} />
+      ))}
     </div>
   );
 }
